c2bbd4b480568f2a44cfdfbc0aafc188d752aeaa
Greenphx9 <55181320+Greenphx9@users.noreply.github.com>
2024-07-23 21:25:06 +1000

    battle_setup.h

--- include/battle_setup.h
+++ include/battle_setup.h
@@ -4,9 +4,11 @@
 #include "global.h"
 
 void StartWildBattle(void);
+void BattleSetup_StartDoubleWildBattle(void);
 void StartRoamerBattle(void);
 void StartOldManTutorialBattle(void);
 void StartScriptedWildBattle(void);
+void BattleSetup_StartScriptedDoubleWildBattle(void);
 void StartMarowakBattle(void);
 void StartSouthernIslandBattle(void);
 void StartLegendaryBattle(void);
@@ -14,6 +16,8 @@ void StartGroudonKyogreBattle(void);
 void StartRegiBattle(void);
 u8 BattleSetup_GetTerrainId(void);
 u8 BattleSetup_GetBattleTowerBattleTransition(void);
+u8 GetWildBattleTransition(void);
+u8 GetTrainerBattleTransition(void);
 const u8 *BattleSetup_ConfigureTrainerBattle(const u8 *data);
 void ConfigureAndSetUpOneTrainerBattle(u8 trainerEventObjId, const u8 *trainerScript);
 bool32 GetTrainerFlagFromScriptPointer(const u8 *data);
@@ -34,6 +38,10 @@ void PlayTrainerEncounterMusic(void);
 const u8 *GetTrainerALoseText(void);
 const u8 *GetTrainerWonSpeech(void);
 
+void DoStandardWildBattle_Debug(void);
 void BattleSetup_StartTrainerBattle_Debug(void);
+s32 TrainerIdToRematchTableId(const struct RematchTrainer *table, u16 trainerId);
+s32 FirstBattleTrainerIdToRematchTableId(const struct RematchTrainer *table, u16 trainerId);
+u16 GetRematchTrainerIdFromTable(const struct RematchTrainer *table, u16 firstBattleTrainerId);
 
 #endif // GUARD_BATTLE_SETUP_H
