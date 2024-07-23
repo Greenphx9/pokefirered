37882c04f3e1cae85877848d6c7c264bc336d5cf
Greenphx9 <55181320+Greenphx9@users.noreply.github.com>
2024-07-23 21:18:28 +1000

    battle_main.h

--- include/battle_main.h
+++ include/battle_main.h
@@ -3,12 +3,7 @@
 
 #include "pokemon.h"
 #include "constants/abilities.h"
-
-struct TrainerMoney
-{
-    u8 classId;
-    u8 value;
-};
+#include "data.h"
 
 struct MultiBattlePokemonTx
 {
@@ -27,29 +22,27 @@ struct MultiBattlePokemonTx
 #define TYPE_NAME_LENGTH 6
 #define ABILITY_NAME_LENGTH ((B_EXPANDED_ABILITY_NAMES == TRUE) ? 16 : 12)
 
-// defines for the u8 array gTypeEffectiveness
-#define TYPE_EFFECT_ATK_TYPE(i)((gTypeEffectiveness[i + 0]))
-#define TYPE_EFFECT_DEF_TYPE(i)((gTypeEffectiveness[i + 1]))
-#define TYPE_EFFECT_MULTIPLIER(i)((gTypeEffectiveness[i + 2]))
-
-// defines for the gTypeEffectiveness multipliers
-#define TYPE_MUL_NO_EFFECT          0
-#define TYPE_MUL_NOT_EFFECTIVE      5
-#define TYPE_MUL_NORMAL             10
-#define TYPE_MUL_SUPER_EFFECTIVE    20
-
-// special type table Ids
-#define TYPE_FORESIGHT  0xFE
-#define TYPE_ENDTABLE   0xFF
-
 // defines for the 'DoBounceEffect' function
 #define BOUNCE_MON          0x0
 #define BOUNCE_HEALTHBOX    0x1
 
+enum {
+    FIRST_TURN_EVENTS_START,
+    FIRST_TURN_EVENTS_OVERWORLD_WEATHER,
+    FIRST_TURN_EVENTS_TERRAIN,
+    FIRST_TURN_EVENTS_STARTING_STATUS,
+    FIRST_TURN_EVENTS_TOTEM_BOOST,
+    FIRST_TURN_EVENTS_NEUTRALIZING_GAS,
+    FIRST_TURN_EVENTS_SWITCH_IN_ABILITIES,
+    FIRST_TURN_EVENTS_OPPORTUNIST_1,
+    FIRST_TURN_EVENTS_ITEM_EFFECTS,
+    FIRST_TURN_EVENTS_OPPORTUNIST_2,
+    FIRST_TURN_EVENTS_END,
+};
+
 extern const struct SpriteTemplate gUnknownDebugSprite;
 extern const struct OamData gOamData_BattlerOpponent;
 extern const struct OamData gOamData_BattlerPlayer;
-extern const u8 gTypeNames[][TYPE_NAME_LENGTH + 1];
 extern const u8 gStatusConditionString_PoisonJpn[8];
 extern const u8 gStatusConditionString_SleepJpn[8];
 extern const u8 gStatusConditionString_ParalysisJpn[8];
@@ -58,9 +51,8 @@ extern const u8 gStatusConditionString_IceJpn[8];
 extern const u8 gStatusConditionString_ConfusionJpn[8];
 extern const u8 gStatusConditionString_LoveJpn[8];
 extern const u8 *const gStatusConditionStringsTable[7][2];
-extern const u8 gTypeEffectiveness[336];
-extern const struct TrainerMoney gTrainerMoneyTable[];
 extern const struct Ability gAbilitiesInfo[ABILITIES_COUNT];
+extern const struct TypeInfo gTypesInfo[NUMBER_OF_MON_TYPES];
 
 void CB2_InitBattle(void);
 void BattleMainCB2(void);
@@ -84,15 +76,28 @@ void SpriteCB_PlayerThrowInit(struct Sprite *sprite);
 void UpdatePlayerPosInThrowAnim(struct Sprite *sprite);
 void BeginBattleIntroDummy(void);
 void BeginBattleIntro(void);
-void SwitchInClearSetData(void);
-void FaintClearSetData(void);
+void SwitchInClearSetData(u32 battler);
+const u8* FaintClearSetData(u32 battler);
 void BattleTurnPassed(void);
-u8 IsRunningFromBattleImpossible(void);
+u8 IsRunningFromBattleImpossible(u32 battler);
+void SwitchTwoBattlersInParty(u32 battler, u32 battler2);
+void SwitchPartyOrder(u32 battlerId);
 void UpdatePartyOwnerOnSwitch_NonMulti(u8 battler);
 void SwapTurnOrder(u8 id1, u8 id2);
-u8 GetWhoStrikesFirst(u8 battler1, u8 battler2, bool8 ignoreChosenMoves);
+u32 GetBattlerTotalSpeedStatArgs(u32 battler, u32 ability, u32 holdEffect);
+u32 GetBattlerTotalSpeedStat(u32 battler);
+s8 GetChosenMovePriority(u32 battlerId);
+s8 GetMovePriority(u32 battlerId, u16 move);
+s32 GetWhichBattlerFasterArgs(u32 battler1, u32 battler2, bool32 ignoreChosenMoves, u32 ability1, u32 ability2,
+                              u32 holdEffectBattler1, u32 holdEffectBattler2, u32 speedBattler1, u32 speedBattler2, s32 priority1, s32 priority2);
+s32 GetWhichBattlerFaster(u32 battler1, u32 battler2, bool32 ignoreChosenMoves);
 void RunBattleScriptCommands_PopCallbacksStack(void);
 void RunBattleScriptCommands(void);
-bool8 TryRunFromBattle(u8 battler);
+void SetTypeBeforeUsingMove(u32 move, u32 battlerAtk);
+bool32 IsWildMonSmart(void);
+u8 CreateNPCTrainerPartyFromTrainer(struct Pokemon *party, const struct Trainer *trainer, bool32 firstTrainer, u32 battleTypeFlags);
+void ModifyPersonalityForNature(u32 *personality, u32 newNature);
+u32 GeneratePersonalityForGender(u32 gender, u32 species);
+void CustomTrainerPartyAssignMoves(struct Pokemon *mon, const struct TrainerMon *partyEntry);
 
 #endif // GUARD_BATTLE_MAIN_H
