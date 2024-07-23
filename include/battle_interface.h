edc1cd2ea9bc708f9a3dd78db8df78cf4fb740f4
Greenphx9 <55181320+Greenphx9@users.noreply.github.com>
2024-07-23 21:15:02 +1000

    battle_interface.h

--- include/battle_interface.h
+++ include/battle_interface.h
@@ -7,7 +7,8 @@
 enum
 {
     HP_CURRENT,
-    HP_MAX
+    HP_MAX,
+    HP_BOTH
 };
 
 enum
@@ -51,6 +52,22 @@ enum
 #define TAG_HEALTHBOX_PAL                       TAG_HEALTHBOX_PLAYER1_TILE
 #define TAG_HEALTHBAR_PAL                       TAG_HEALTHBAR_PLAYER1_TILE
 
+#define TAG_MEGA_TRIGGER_TILE           0xD777
+#define TAG_MEGA_INDICATOR_TILE         0xD778
+#define TAG_ALPHA_INDICATOR_TILE        0xD779
+#define TAG_OMEGA_INDICATOR_TILE        0xD77A
+#define TAG_ZMOVE_TRIGGER_TILE          0xD77B
+#define TAG_BURST_TRIGGER_TILE          0xD77C
+#define TAG_DYNAMAX_TRIGGER_TILE        0xD77D
+#define TAG_DYNAMAX_INDICATOR_TILE      0xD77E
+
+#define TAG_MEGA_TRIGGER_PAL            0xD777
+#define TAG_MEGA_INDICATOR_PAL          0xD778
+#define TAG_MISC_INDICATOR_PAL          0xD779 // Alpha, Omega, and Dynamax indicators use the same palette as each of them only uses 4 different colors.
+#define TAG_ZMOVE_TRIGGER_PAL           0xD77B
+#define TAG_BURST_TRIGGER_PAL           0xD77C
+#define TAG_DYNAMAX_TRIGGER_PAL         0xD77D
+
 enum
 {
     HEALTHBOX_ALL,
@@ -67,18 +84,31 @@ enum
     HEALTHBOX_SAFARI_BALLS_TEXT
 };
 
+u32 WhichBattleCoords(u32 battlerId);
 void Task_HidePartyStatusSummary(u8 taskId);
 u8 CreateBattlerHealthboxSprites(u8 battlerId);
 u8 CreateSafariPlayerHealthboxSprites(void);
 void SetBattleBarStruct(u8 battlerId, u8 healthboxSpriteId, s32 maxVal, s32 currVal, s32 receivedValue);
 void SetHealthboxSpriteInvisible(u8 healthboxSpriteId);
 void SetHealthboxSpriteVisible(u8 healthboxSpriteId);
-void DestoryHealthboxSprite(u8 healthboxSpriteId);
 void DummyBattleInterfaceFunc(u8 healthboxSpriteId, bool8 isDoubleBattleBankOnly);
-void UpdateOamPriorityInAllHealthboxes(u8 priority);
+void UpdateOamPriorityInAllHealthboxes(u8 priority, bool32 hideHpBoxes);
 void InitBattlerHealthboxCoords(u8 battlerId);
-void UpdateHpTextInHealthbox(u8 healthboxSpriteId, s16 value, u8 maxOrCurrent);
+void GetBattlerHealthboxCoords(u8 battler, s16 *x, s16 *y);
+void UpdateHpTextInHealthbox(u32 healthboxSpriteId, u32 maxOrCurrent, s16 currHp, s16 maxHp);
 void SwapHpBarsWithHpText(void);
+void ChangeMegaTriggerSprite(u8 spriteId, u8 animId);
+void CreateMegaTriggerSprite(u8 battlerId, u8 palId);
+bool32 IsMegaTriggerSpriteActive(void);
+void HideMegaTriggerSprite(void);
+void DestroyMegaTriggerSprite(void);
+void ChangeBurstTriggerSprite(u8 spriteId, u8 animId);
+void CreateBurstTriggerSprite(u8 battlerId, u8 palId);
+bool32 IsBurstTriggerSpriteActive(void);
+void HideBurstTriggerSprite(void);
+void DestroyBurstTriggerSprite(void);
+void MegaIndicator_LoadSpritesGfx(void);
+void MegaIndicator_SetVisibilities(u32 healthboxId, bool32 invisible);
 u8 CreatePartyStatusSummarySprites(u8 battlerId, struct HpAndStatus *partyInfo, u8 isSwitchingMons, bool8 isBattleStart);
 void UpdateHealthboxAttribute(u8 healthboxSpriteId, struct Pokemon *mon, u8 elementId);
 u8 GetScaledHPFraction(s16 hp, s16 maxhp, u8 scale);
@@ -87,5 +117,14 @@ void UpdateNickInHealthbox(u8 spriteId, struct Pokemon *mon);
 void TryAddPokeballIconToHealthbox(u8 spriteId, u8);
 s32 MoveBattleBar(u8 battler, u8 healthboxSpriteId, u8 whichBar, u8 arg3);
 void CreateAbilityPopUp(u8 battlerId, u32 ability, bool32 isDoubleBattle);
+void DestroyAbilityPopUp(u8 battlerId);
+void HideTriggerSprites(void);
+bool32 CanThrowLastUsedBall(void);
+void TryHideLastUsedBall(void);
+void TryRestoreLastUsedBall(void);
+void TryAddLastUsedBallItemSprites(void);
+void SwapBallToDisplay(bool32 sameBall);
+void ArrowsChangeColorLastBallCycle(bool32 showArrows);
+void UpdateAbilityPopup(u8 battlerId);
 
 #endif // GUARD_BATTLE_INTERFACE_H
